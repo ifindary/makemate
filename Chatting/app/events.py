61a13dadf39be88ca0bac3ec49cdb5cabfd1c52a
yerim <1000gaji@gmail.com>
2024-01-11 06:51:47 +0900

    room id 전달

--- Chatting/app/events.py
+++ Chatting/app/events.py
@@ -6,8 +6,8 @@ import random
 
 from pymongo import MongoClient
 
-client = MongoClient('localhost', 27017)
-db = client.save_chat #테이블 이름 - 컬렉션 상위
+room_client = MongoClient('localhost', 27017)
+db = room_client.save_chat #테이블 이름 - 컬렉션 상위
 #room 정보 - room_data 컬렉션
 #chat 정보 - room_msg 컬렉션
 
@@ -22,41 +22,37 @@ def socketio_init(socketio):
         
         user_img = message['img']
         user1 = message['user1']
-        user2 = message['user2']
+        genre = message['genre']
+        room_id = message['room_id']
         
-        print(user1, 'joind', user2)
+        print(genre)
         
         #is_room = db.room_data.find_one({  })
         
         # 재입장 고려하지 않음
-        while(True):
-            room = random.randint(1000, 9999)
-            room = str(room)
-            
-            print(room + " 넘버가 생성되었씁니다.")
-            result = db.room_data.find_one({'room_id': room})
-            
-            # 중복되지 않으면 null 반환
-            if result is None:
-                print("room create!")
-                break;
         
-        join_room(room)
-        session['room'] = room
+        # 생성이 되어있는지 확인
+        result = db.room_data.find_one({'room_id': room_id})
+            
+        # 중복되지 않으면 null 반환
+        if result is None:
+            print("room create!")
+        
+        join_room(room_id)
+        session['room'] = room_id
         
         data = {
-            'room_id': room,
+            'room_id': room_id,
             'user1': user1,
-            'user2': user2,
-            #'genre': '',
+            'genre': genre,
         }
         db.room_data.insert_one(data)
         
-        emit('status', {'msg':user1 + '님이 입장하셧습니다'}, room=room)
+        emit('status', {'msg':user1 + '님이 입장하셧습니다'}, room=room_id)
           
     @socketio.on('text', namespace='/chat')
     def text(message):
-        room = session.get('room')
+        room = session.get('room_id')
         sender = session.get('user1')
         date = datetime.datetime.now()
         
@@ -72,9 +68,14 @@ def socketio_init(socketio):
         
         emit('message', {'msg' : sender + ':' + message['msg']}, room=room)
         
+    #@socketio.on('connect', namespace='/')
+    #def connect_handler(data):
+    #    print(data)
+        
+        
     @socketio.on('left', namespace='/chat')
     def left(message):
-        room = session.get('room')
+        room = session.get('room_id')
         leave_room(room)
         emit('status', {'msg':session.get('name') + '님이 퇴장'}, room=room)
         
--- Chatting/app/routes.py
+++ Chatting/app/routes.py
@@ -6,17 +6,21 @@ main = Blueprint('main', __name__)
 def moveroom():
     session['img'] = request.args.get('img')
     session['user1'] = request.args.get('user1')
-    session['user2'] = request.args.get('user2')
+    #session['user2'] = request.args.get('user2')
+    session['genre'] = request.args.get('genre')
+    session['room_id'] = request.args.get('room_id')
     
     return redirect(url_for('.chatroom'))
 
 @main.route('/chatting')
 def chatroom():
     user1 = session.get('user1', '')
-    user2 = session.get('user2', '')
+    #user2 = session.get('user2', '')
+    genre = session.get('genre', '')
     img = session.get('img', '')
+    room_id = session.get('room_id', '')
     
-    print(user1 + ' & ' + user2 + ' & ' +img)
-    if user1 == '' or user2 == '':
+    #print(user1 + ' & ' + user2 + ' & ' +img)
+    if user1 == '':
         return redirect(url_for('.moveroom')) # 라우트 함수이름
-    return render_template('chatroom.html', user1=user1, user2=user2, img=img)#, 200, {'Content-Type': 'text/html; charset=utf-8'}
+    return render_template('chatroom.html', user1=user1, genre=genre, img=img, room_id=room_id)#, 200, {'Content-Type': 'text/html; charset=utf-8'}
